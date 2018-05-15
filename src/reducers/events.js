971b8d529991502f923cc60a4e148e2fa78bdde4
Eva Michalcak <eva.michalcak@gmail.com>
2018-05-15 18:57:19 +0200

    state with idsByFilter

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -15,38 +15,76 @@ const byId = (state={}, action) => {
 };
 
 const allIds = (state=[], action) => {
+  if (action.filter !== 'SHOW_ALL') {
+    return state;
+  }
   switch (action.type) {
-    case 'ADD_EVENT':
-      return [...state, action.id];
+    case 'RECEIVE_EVENTS':
+      return action.response.map(todo => todo.id);
     default:
       return state;
   }
 };
 
+const activeIds = (state=[], action) => {
+  if (action.filter !== 'SHOW_ACTIVE') {
+    return state;
+  }
+  switch (action.type) {
+    case 'RECEIVE_EVENTS':
+      return action.response.map(todo => todo.id);
+    default:
+      return state;
+  }
+};
+
+const completedIds = (state=[], action) => {
+  if (action.filter !== 'SHOW_COMPLETED') {
+    return state;
+  }
+  switch (action.type) {
+    case 'RECEIVE_EVENTS':
+      return action.response.map(todo => todo.id);
+    default:
+      return state;
+  }
+};
+
+const idsByFilter = combineReducers({
+  SHOW_ALL: allIds,
+  SHOW_ACTIVE: activeIds, 
+  SHOW_COMPLETED: completedIds,
+});
+
 const events = combineReducers({
   byId,
-  allIds,
+  idsByFilter,
 });
 
 export default events;
 
-const getAllEvents = (state) => 
-  state.allIds.map(id => state.byId[id]);
+// const getAllEvents = (state) => 
+//   state.allIds.map(id => state.byId[id]);
 
 export const getVisibleEvents = (state, filter) => {
-  const allEvents = getAllEvents(state);
-  switch (filter) {
-    case 'SHOW_ALL': 
-      return allEvents;
-    case 'SHOW_COMPLETED':
-      return allEvents.filter(
-        t => t.completed
-      );
-    case 'SHOW_ACTIVE':
-      return allEvents.filter(
-        t => !t.completed
-      );
-    default:
-      throw new Error(`Unknown filter: ${filter}.`);
-  }
+  const ids = state.idsByFilter[filter];
+  return ids.map(id => state.byId[id]);
 }
+
+// export const getVisibleEvents = (state, filter) => {
+//   const allEvents = getAllEvents(state);
+//   switch (filter) {
+//     case 'SHOW_ALL': 
+//       return allEvents;
+//     case 'SHOW_COMPLETED':
+//       return allEvents.filter(
+//         t => t.completed
+//       );
+//     case 'SHOW_ACTIVE':
+//       return allEvents.filter(
+//         t => !t.completed
+//       );
+//     default:
+//       throw new Error(`Unknown filter: ${filter}.`);
+//   }
+// }
